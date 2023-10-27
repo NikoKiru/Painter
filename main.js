eb0a84135c39b21f1b4ccda18a9ada3b0cadeb2e
Nikolaj <dknikolajk@gmail.com>
2023-10-27 10:23:27 +0200

    Final

--- main.js
+++ main.js
@@ -6,17 +6,24 @@ const canvas = document.querySelector("canvas"),
     colorPicker = document.querySelector("#color-picker"),
     clearCanvas = document.querySelector(".clear-canvas"),
     saveImg = document.querySelector(".save-img"),
+    undo = document.querySelector(".undo"),
+    redo = document.querySelector(".redo"),
     ctx = canvas.getContext("2d");
 
+
+// Creates array of currentstates
 let history = [];
 let currentState = -1;
 
+//Starting conditions for loading page
 let prevMouseX, prevMouseY, snapshot,
 isDrawing = false,
 selectedTool ="brush",
 brushWidth = 5,
 selectedColor = "#000";
 
+
+//Sets starting canvas
 const setCanvasBackground = () => {
     ctx.fillStyle = "#fff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -25,13 +32,13 @@ const setCanvasBackground = () => {
     history.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
     currentState++;
 }
-
+//Makes the mouse alling with the drawing on canvas
 window.addEventListener("load", () =>{
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
     setCanvasBackground();
 })
-
+//Rectangle drawing function
 const drawRect = (e) => {
     if (!fillColor.checked) {
         return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
@@ -39,7 +46,7 @@ const drawRect = (e) => {
     ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
 }
 
-
+//Circle drawing function
 const drawCircle = (e) => {
     ctx.beginPath();
     let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
@@ -47,6 +54,7 @@ const drawCircle = (e) => {
     fillColor.checked ?ctx.fill() : ctx.stroke();
 }
 
+//Triangle drawing fuction
 const drawTriangle = (e) => {
     ctx.beginPath();
     ctx.moveTo(prevMouseX, prevMouseY);
@@ -56,7 +64,7 @@ const drawTriangle = (e) => {
     fillColor.checked ?ctx.fill() : ctx.stroke();
 }
 
-
+//drawing function
 const startDraw = (e) => {
     isDrawing = true;
     prevMouseX = e.offsetX;
@@ -70,7 +78,7 @@ const startDraw = (e) => {
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
-
+//Determine which function to use when drawing
 const drawing = (e) => {
     if (!isDrawing) return;
     ctx.putImageData(snapshot, 0, 0);
@@ -87,21 +95,21 @@ const drawing = (e) => {
         drawTriangle(e);
     }
 }
-
+//Undo
 const undoLastAction = () => {
     if (currentState > 0) {
         currentState--;
         ctx.putImageData(history[currentState], 0, 0);
     }
 }
-
+//redo
 const redoLastAction = () => {
     if (currentState < history.length - 1) {
         currentState++;
         ctx.putImageData(history[currentState], 0, 0);
     }
 }
-
+//Selects the tool and makes it active
 toolBtns.forEach(btn => {
     btn.addEventListener("click", () => {
 
@@ -112,8 +120,10 @@ toolBtns.forEach(btn => {
     });
 });
 
+//Sizeslider for brush width
 sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value);
 
+//Color button
 colorBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         document.querySelector(".options .selected").classList.remove("selected");
@@ -121,12 +131,12 @@ colorBtns.forEach(btn => {
         selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
     });
 });
-
+//the color picker
 colorPicker.addEventListener("change", () => {
     colorPicker.parentElement.style.background = colorPicker.value;
     colorPicker.parentElement.click();
 });
-
+//Clear canvas function
 clearCanvas.addEventListener("click", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     setCanvasBackground();
@@ -135,28 +145,34 @@ clearCanvas.addEventListener("click", () => {
     history.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
     currentState++;
 });
-
+//Save as image function
 saveImg.addEventListener("click", () => {
     const link = document.createElement("a");
     link.download = `${Date.now()}.jpg`;
     link.href = canvas.toDataURL();
     link.click();
 });
-
-document.querySelector(".undo").addEventListener("click", undoLastAction);
+//undo
+undo.addEventListener("click", undoLastAction);
 
 document.addEventListener("keydown", function (event){
     if (event.ctrlKey && event.key === "z") {
         undoLastAction();
     }
 });
+//redo
+redo.addEventListener("click", redoLastAction);
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey && event.key === "x") {
+        redoLastAction();
+    }
+});
 
-document.querySelector(".redo").addEventListener("click", redoLastAction);
-
-
-
+//Start draw
 canvas.addEventListener("mousedown", startDraw);
+//Draw
 canvas.addEventListener("mousemove", drawing);
+//Stop draw and save to history
 canvas.addEventListener("mouseup", () => {
     isDrawing = false;
 
